dabb97c08c455d509cedea4f4e7095134d4ba8c8
Danielle Maier <daniellemaier@Danielles-MacBook-Pro.local>
2017-06-26 12:42:54 -0600

    most styling done

--- README.md
+++ README.md
@@ -2,5 +2,8 @@ Readme for Dani's Restaurant Roulette web app.
 
 I have no idea what to write here, so this will take up space.
 
-https://wireframe.cc/FNwsp8
-first draft
+Wireframe:
+https://wireframe.cc/FNwsp8 - first draft
+
+Trello board:
+https://trello.com/b/m6FEISoz/restaurant-roulette
--- scripts/app.js
+++ scripts/app.js
@@ -13,7 +13,7 @@ function geoFindMe() {
     output.innerHTML = '<p>Latitude is ' + latitude + '° <br>Longitude is ' + longitude + '°</p>';
 
     var img = new Image();
-    img.src = "https://maps.googleapis.com/maps/api/staticmap?center=" + latitude + "," + longitude + "&zoom=13&size=300x300&sensor=false";
+    img.src = "https://maps.googleapis.com/maps/api/staticmap?center=" + latitude + "," + longitude + "&zoom=13&size=250x250&sensor=false";
 
     output.appendChild(img);
   }
